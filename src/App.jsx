71c958c18e67adc1800c3e92b28761af840effc1
Antaryami Sahoo <mail2sahooantaryami@gmail.com>
2024-08-04 15:55:50 +0530

    Added add to cart feature on the product details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { PageNotFound } from "components/commons";
 import { Route, Switch } from "react-router-dom";
@@ -7,24 +7,21 @@ import routes from "routes";
 
 import Product from "./components/Product/Product";
 import ProductList from "./components/ProductList/ProductList";
+import CartItemsContext from "./contexts/CartItemsContext";
 
-const App = () => (
-  <>
-    {/* <div className="mx-4 flex space-x-2">
-      <NavLink exact activeClassName="underline font-bold" to="/">
-        Home
-      </NavLink>
-      <NavLink exact activeClassName="underline font-bold" to="/product">
-        Product
-      </NavLink>
-    </div> */}
-    <Switch>
-      <Route exact component={Product} path={routes.products.show} />
-      <Route exact component={ProductList} path={routes.products.index} />
-      <Redirect exact from={routes.root} to={routes.products.index} />
-      <Route component={PageNotFound} path="*" />
-    </Switch>
-  </>
-);
+const App = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  return (
+    <CartItemsContext.Provider value={[cartItems, setCartItems]}>
+      <Switch>
+        <Route exact component={Product} path={routes.products.show} />
+        <Route exact component={ProductList} path={routes.products.index} />
+        <Redirect exact from={routes.root} to={routes.products.index} />
+        <Route component={PageNotFound} path="*" />
+      </Switch>
+    </CartItemsContext.Provider>
+  );
+};
 
 export default App;
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -5,7 +5,7 @@ import productsApi from "apis/products";
 import Header from "components/commons/Header";
 import useDebounce from "hooks/useDebounce";
 import { Spinner, NoData, Input } from "neetoui";
-import { isEmpty, without } from "ramda";
+import { isEmpty } from "ramda";
 
 import ProductListItem from "./ProductListItem";
 
@@ -13,7 +13,6 @@ const ProductList = () => {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [searchKey, setSearchKey] = useState("");
-  const [cartItems, setCartItems] = useState([]);
   const debouncedSearchKey = useDebounce(searchKey);
 
   const fetchProducts = async () => {
@@ -26,13 +25,6 @@ const ProductList = () => {
       setIsLoading(false);
     }
   };
-
-  const toggleIsInCart = slug =>
-    setCartItems(prevCartItems =>
-      prevCartItems.includes(slug)
-        ? without([slug], cartItems)
-        : [slug, ...cartItems]
-    );
 
   useEffect(() => {
     fetchProducts();
@@ -49,7 +41,6 @@ const ProductList = () => {
   return (
     <div className="flex h-screen flex-col">
       <Header
-        cartItemsCount={cartItems.length}
         shouldShowBackButton={false}
         title="Smile cart"
         actionBlock={
@@ -67,12 +58,7 @@ const ProductList = () => {
       ) : (
         <div className="grid grid-cols-2 justify-items-center gap-y-8 p-4 md:grid-cols-3 lg:grid-cols-4">
           {products.map(product => (
-            <ProductListItem
-              key={product.slug}
-              {...product}
-              isInCart={cartItems.includes(product.slug)}
-              toggleIsInCart={() => toggleIsInCart(product.slug)}
-            />
+            <ProductListItem key={product.slug} {...product} />
           ))}
         </div>
       )}
